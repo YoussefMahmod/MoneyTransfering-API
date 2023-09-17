79efd18b238244738603b8808b0b796ef24d5ac7
YoussefMahmod <youss3fmahmoud@gmail.com>
2023-09-17 15:59:12 +0300

    [V1][feat][ACCOUNT] add account model to datastore

--- store/datastore.go
+++ store/datastore.go
@@ -1,1 +1,21 @@
 package store
+
+import (
+	"moneytransfer-api/models"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type Datastore struct {
+	AccountsByID map[uuid.UUID]models.IAccount
+	Mutex        sync.RWMutex
+}
+
+func NewDatastore() *Datastore {
+	return &Datastore{
+		// Index on Accounts(ID)
+		AccountsByID: make(map[uuid.UUID]models.IAccount),
+		Mutex:        sync.RWMutex{},
+	}
+}
