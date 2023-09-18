c362026010e93bba41004fc350fc2f7ab80bf0ce
YoussefMahmod <youss3fmahmoud@gmail.com>
2023-09-19 02:37:08 +0300

    [V1][FEAT][TRANSACTION] add txn services(both inside accounts, transactions).

--- services/transaction.go
+++ services/transaction.go
@@ -1,1 +1,44 @@
 package services
+
+import (
+	"github.com/YoussefMahmod/MoneyTransfering-API/models"
+	"github.com/YoussefMahmod/MoneyTransfering-API/store"
+	"github.com/google/uuid"
+)
+
+type TransactionsServiceHandler struct {
+	store *store.Datastore
+}
+
+func NewTransactionsServiceHandler(store *store.Datastore) *TransactionsServiceHandler {
+	return &TransactionsServiceHandler{store: store}
+}
+
+func (svc *TransactionsServiceHandler) GetAll() []models.ITransaction {
+	transactions := make([]models.ITransaction, svc.store.TransactionsByID.Count())
+	data := svc.store.TransactionsByID.GetAll()
+
+	i := 0
+	for k := range transactions {
+		transactions[i] = data[k].(models.ITransaction)
+		i++
+	}
+
+	return transactions
+}
+
+func (svc *TransactionsServiceHandler) GetOneByID(id uuid.UUID) (interface{}, bool) {
+	transactionByID, exists := svc.store.TransactionsByID.Get(id)
+	return transactionByID, exists
+}
+
+func (svc *TransactionsServiceHandler) DelOneByID(id uuid.UUID) bool {
+	_, exist := svc.store.TransactionsByID.Del(id)
+
+	if !exist {
+		return false
+	}
+
+	svc.store.TransactionsByID.Del(id)
+	return true
+}
