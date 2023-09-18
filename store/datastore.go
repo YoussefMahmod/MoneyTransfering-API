405a7bc1bbf7fca22df93c76bec8ef6b601c901f
YoussefMahmod <youss3fmahmoud@gmail.com>
2023-09-19 02:35:58 +0300

    [V1][FEAT][TRANSACTION] add txn to datastore and implement indices(ID, Sender, Reciever).

--- store/datastore.go
+++ store/datastore.go
@@ -1,20 +1,27 @@
 package store
 
 import (
-	"sync"
-
+	"github.com/YoussefMahmod/MoneyTransfering-API/models"
 	customtypes "github.com/YoussefMahmod/MoneyTransfering-API/utils/custom_types"
+	"github.com/google/uuid"
 )
 
 type Datastore struct {
-	AccountsByID customtypes.ShardMap
-	Mutex        sync.RWMutex
+	AccountsByID           customtypes.ShardMap
+	TransactionsByID       customtypes.ShardMap
+	TransactionsBySender   map[uuid.UUID][]models.ITransaction
+	TransactionsByReciever map[uuid.UUID][]models.ITransaction
 }
 
 func NewDatastore() *Datastore {
 	return &Datastore{
 		// Index on Accounts(ID)
 		AccountsByID: *customtypes.NewShardMap(16),
-		Mutex:        sync.RWMutex{},
+		// Index on Transactions(ID)
+		TransactionsByID: *customtypes.NewShardMap(16),
+		// Index on TransactionsBySender(ID)
+		TransactionsBySender: make(map[uuid.UUID][]models.ITransaction),
+		// Index on TransactionsByReciver(ID)
+		TransactionsByReciever: make(map[uuid.UUID][]models.ITransaction),
 	}
 }
