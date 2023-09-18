151c9e4ae755b465858c0c8188e9ea8fa04df196
YoussefMahmod <youss3fmahmoud@gmail.com>
2023-09-18 16:46:25 +0300

    [V1][enhance][ACCOUNT] refactor the api to reflect models changes.

--- api/account.go
+++ api/account.go
@@ -1,14 +1,11 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"moneytransfer-api/models"
 	"moneytransfer-api/services"
-	"moneytransfer-api/utils"
 	"net/http"
-	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -23,7 +20,7 @@ func (u Account) router(server *Server, svcHandler *services.AccountsServiceHand
 	u.server = server
 	u.serviceHandler = svcHandler
 
-	serverGroup := server.router.Group("/accounts")
+	serverGroup := server.Router.Group("/accounts")
 	serverGroup.GET("/", u.getAccountsList)
 	serverGroup.GET("/:id", u.getAccount)
 	serverGroup.POST("/", u.createAccount)
@@ -32,7 +29,6 @@ func (u Account) router(server *Server, svcHandler *services.AccountsServiceHand
 	serverGroup.DELETE("/:id", u.delAccount)
 }
 
-// TODO: split them into chunks and get each chunk concurrently
 func (a *Account) getAccountsList(c *gin.Context) {
 	accounts := a.serviceHandler.GetAll()
 
@@ -58,60 +54,56 @@ func (a *Account) getAccount(c *gin.Context) {
 }
 
 func (a *Account) createAccount(c *gin.Context) {
-	account := models.NewAccount()
-
-	if err := c.ShouldBindJSON(&account); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	a.serviceHandler.InsertOne(&account)
-
-	c.JSON(http.StatusCreated, account)
-}
-
-// TODO: split them into chunks and insert each chunk concurrently
-func (a *Account) createAccountsInBulk(c *gin.Context) {
-	accounts := models.NewListAccounts()
-
 	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	err = json.Unmarshal(body, &accounts)
-
+	account, err := models.NewAccount(body)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	var wg sync.WaitGroup
-	chunk_length := len(accounts) / 10 // TODO: make it configurable
-	IAccounts := make([]models.IAccount, 0)
+	a.serviceHandler.InsertOne(account)
 
-	for i := 0; i < len(accounts); i += chunk_length {
-		wg.Add(1)
-		go func(l int, r int) {
-			for ; l < r; l++ {
-				accounts[l].SetDefaults()
-				IAccounts = append(IAccounts, &accounts[l])
-			}
-			wg.Done()
-		}(i, utils.Min(i+chunk_length, len(IAccounts)-1))
+	c.JSON(http.StatusCreated, account)
+}
+
+func (a *Account) createAccountsInBulk(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
-	wg.Wait()
 
-	a.serviceHandler.InsertMany(IAccounts)
+	accounts, err := models.NewListAccounts(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-	Logger.Info(fmt.Sprintf("INFO - %v accounts are ingested and ready to transfer", len(IAccounts)))
+	a.serviceHandler.InsertMany(accounts)
+
+	Logger.Info(fmt.Sprintf("INFO - %v accounts are ingested and ready to transfer", len(accounts)))
 
 	c.JSON(http.StatusCreated, accounts)
 }
 
 func (a *Account) patchAccount(c *gin.Context) {
-	account := models.NewAccount()
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	account, err := models.NewAccount(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	id := c.Param("id")
 	uuid, err := uuid.Parse(id)
 	if err != nil {
@@ -119,12 +111,7 @@ func (a *Account) patchAccount(c *gin.Context) {
 		return
 	}
 
-	if err := c.ShouldBindJSON(&account); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	newAccount, err := a.serviceHandler.PatchOneByID(uuid, &account)
+	newAccount, err := a.serviceHandler.PatchOneByID(uuid, account)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
